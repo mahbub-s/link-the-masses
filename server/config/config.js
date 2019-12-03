bf42f0ec0b653b3789aedad7f31323b63b6c9dbe
bazrith <sahnon.mahbub@gmail.com>
2019-12-02 20:53:42 -0500

    key and password removal

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-    JWT_KEY: process.env.JWT_KEY || 'secretKey',
-    MONGO_ATLAS_PW: process.env.MONGO_ATLAS_PW || 'admin'
+    JWT_KEY: process.env.JWT_KEY,
+    MONGO_ATLAS_PW: process.env.MONGO_ATLAS_PW
   };
